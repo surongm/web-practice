aa89d6d6fa2d423337b8c0c95e1f3901c895e73f
surongm <1137747302@qq.com>
2021-03-18 21:06:03 +0800

    test

--- antd-course-advance/src/pages/HelloWorld.js
+++ antd-course-advance/src/pages/HelloWorld.js
@@ -9,24 +9,26 @@ const HelloWorld = () => {
         border: '1px solid #e8e8e8'
     }
 
-    return (
-        <div>
-            Hello World
-            <Card
-                style={style}
-                actions={[<a>操作一</a>, <a>操作二</a>]}
+    return ( <
+        div >
+        Hello World <
+        Card style = { style }
+        actions = {
+            [ < a > 操作一 < /a>, <a>操作二</a > ] } >
+        <
+        Card.Meta avatar = { < img
+            alt = ''
+            style = {
+                { width: '64px', height: '64px', borderRadius: '32px' } }
+            src = "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png" /
             >
-                <Card.Meta
-                    avatar={<img
-                        alt=''
-                        style={{ width: '64px', height: '64px', borderRadius: '32px' }}
-                        src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
-                    />}
-                    title="Alipay"
-                    description='在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-                />
-            </Card>
-        </div>
+        }
+        title = "Alipay"
+        description = '1在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。' /
+        >
+        <
+        /Card> <
+        /div>
     )
 }
 
